5c2400ed0efb251692c57ad857907ecf374a5dae
柳清水 <151805802+liuqingshuige@users.noreply.github.com>
2024-11-23 11:04:20 +0800

    Update resampler.cpp

--- resampler.cpp
+++ resampler.cpp
@@ -2,7 +2,7 @@
  * 音频重采样封装：libsamplerate实现
  * Copyright FreeCode. All Rights Reserved.
  * MIT License (https://opensource.org/licenses/MIT)
- * 2024 by lingqingshuige
+ * 2024 by liuqingshuige
  */
 #include <stdio.h>
 #include <stdlib.h>
